494f13cdd5d59680e0be8c0aa23bfc59cacfd285
Trisn0802 <trisnawisesa20@gmail.com>
2026-02-11 17:28:45 +0700

    Add HTTPS auto header

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,15 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        if (app()->environment('local')) {
+        	URL::forceScheme('http');
+    	} else {
+    	    URL::forceScheme('https');
+    	}
+
+        // if (request()->header('x-forwarded-proto') === 'https') {
+        //     URL::forceScheme('https');
+        // }
     }
+
 }
