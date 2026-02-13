282b9e1a26994e9c297b929ae0fdc4b6bc38d4f8
Trisn0802 <trisnawisesa20@gmail.com>
2026-02-13 20:05:23 +0700

    delete pusher online (Revert)

--- resources/views/backend/v_layouts/app.blade.php
+++ resources/views/backend/v_layouts/app.blade.php
@@ -677,8 +677,8 @@
         }
     </script>
 
-    {{-- <script src="https://js.pusher.com/8.4.0/pusher.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/laravel-echo/1.15.0/echo.iife.js"></script> --}}
+    <script src="https://js.pusher.com/8.4.0/pusher.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/laravel-echo/1.15.0/echo.iife.js"></script>
     <script>
         @php
             $role = $authUser ? $authUser->role : null;
